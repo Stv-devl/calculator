ffdcad3900bbcf8ab23906fa694ebc66335d6021
Stevan-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-10-11 12:47:10 +0700

    Add files via upload

--- JS/switcher.js
+++ JS/switcher.js
@@ -1,28 +1,51 @@
 //variables
-const theme1btn = document.querySelector(".theme1btn");
-const theme2btn = document.querySelector(".theme2btn");
+const light = document.querySelector(".light");
+const dark = document.querySelector(".dark");
 const theme3btn = document.querySelector(".theme3btn");
-const body = document.querySelector(".theme");
+const body = document.querySelector("body");
 
-//theme buttons
-theme1btn.addEventListener("click", (e) => {
-  e.target.classList.add("theme1clicked");
-  theme2btn.classList.remove("theme2clicked");
+//function for prefer color sheme
+function starterTheme() {
+  const preferLight = window.matchMedia("(prefers-color-scheme: light)");
+  const preferDark = window.matchMedia("(prefers-color-scheme: dark)");
+  const noPreference = window.matchMedia(
+    "(prefers-color-scheme: no-preference)"
+  );
+
+  if (preferLight.matches == true) {
+    body.classList.add("light");
+    light.classList.add("lightclicked");
+  } else if (preferDark.matches == true) {
+    body.classList.add("dark");
+    dark.classList.add("darkclicked");
+  } else {
+    body.classList.add("light");
+    light.classList.add("lightclicked");
+  }
+}
+starterTheme();
+
+//theme 1 : light button
+light.addEventListener("click", () => {
+  light.classList.add("lightclicked");
+  dark.classList.remove("darkclicked");
   theme3btn.classList.remove("theme3clicked");
-  body.classList.add("theme1");
-  body.classList.remove("theme2", "theme3");
+  body.classList.add("light");
+  body.classList.remove("dark", "theme3");
 });
-theme2btn.addEventListener("click", (e) => {
-  e.target.classList.add("theme2clicked");
-  theme1btn.classList.remove("theme1clicked");
+//theme 1 : dark button
+dark.addEventListener("click", () => {
+  dark.classList.add("darkclicked");
+  light.classList.remove("lightclicked");
   theme3btn.classList.remove("theme3clicked");
-  body.classList.add("theme2");
-  body.classList.remove("theme1", "theme3");
+  body.classList.add("dark");
+  body.classList.remove("light", "theme3");
 });
-theme3btn.addEventListener("click", (e) => {
-  e.target.classList.add("theme3clicked");
-  theme1btn.classList.remove("theme1clicked");
-  theme2btn.classList.remove("theme2clicked");
+//theme 1 : button
+theme3btn.addEventListener("click", () => {
+  theme3btn.classList.add("theme3clicked");
+  light.classList.remove("lightclicked");
+  dark.classList.remove("darkclicked");
   body.classList.add("theme3");
-  body.classList.remove("theme1", "theme2");
+  body.classList.remove("light", "dark");
 });
